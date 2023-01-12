fbdde8b1547d3f5bdf78d946891f437fca940c45
Roel010 <roel.webb.v.deventer@gmail.com>
2023-01-12 12:29:15 +0100

    added uniform sampling

--- informed_sampling.py
+++ informed_sampling.py
@@ -1,5 +1,12 @@
 import numpy as np
 
+def sampling_sphere():
+    sample = [2*np.random.random_sample()-1,2*np.random.random_sample()-1,2*np.random.random_sample()-1]
+    if (sample[0]**2 + sample[1]**2 + sample[2]**2 < 1.0):
+        return sample
+    return sampling_sphere()
+
+
 def informed_sample(x_start,x_goal,c_best):
     #prepare
     c_min = np.linalg.norm(x_start-x_goal)
@@ -21,14 +28,14 @@ def informed_sample(x_start,x_goal,c_best):
 
 
     #step2unified sample in ball radius=1
-    r = np.random.random_sample()
+    r = 1#np.random.random_sample()
     theta = 2 * np.pi * np.random.random_sample()
     phi = np.arccos(1 - 2 * np.random.random_sample())
     x = r*np.sin(phi) * np.cos(theta)
     y = r*np.sin(phi) * np.sin(theta)
     z = r*np.cos(phi)
 
-    sample = [x,y,z]
+    sample = sampling_sphere()
     new_node = sample
 
     r1=c_best/2
@@ -37,7 +44,7 @@ def informed_sample(x_start,x_goal,c_best):
     L=np.diag([r1,r2,r3])
     
     #step3 transform unified sample into world frame
-    ellipsoid=np.matmul(np.matmul(C,L),sample)
+    ellipsoid= np.matmul(np.matmul(C,L),sample)
     node=[0,0,0]
     node[0]=ellipsoid[0]+x_centre[0]
     node[1]=ellipsoid[1]+x_centre[1]
